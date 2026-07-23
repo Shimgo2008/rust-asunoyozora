58a3e9904bf25e217dbe998104115d077a6ce2f9
Shimgo2008 <Shimgo2008@gmail.com>
2026-07-23 11:00:45 +0900

    fix: iroiro

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 mod core;
 
-use core::{STDATM, asunoyozora, Meter, Kilogram, Second};
+use core::{asunoyozora, Kilogram, Meter, Second, STDATM};
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
-use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct AsunoyozoraResult {
@@ -26,12 +26,7 @@ pub fn asunoyozora_wasm(
     m: f64,
     h_width_rate: f64,
 ) -> Result<JsValue, JsValue> {
-    let stdatm = STDATM::new(
-        Meter(h_t),
-        Meter(h_h),
-        Kilogram(m),
-        h_width_rate,
-    );
+    let stdatm = STDATM::new(Meter(h_t), Meter(h_h), Kilogram(m), h_width_rate);
 
     let results = asunoyozora(
         Second(t_min),
@@ -41,11 +36,16 @@ pub fn asunoyozora_wasm(
         &stdatm,
     );
 
-    let js_results: Vec<AsunoyozoraResult> = results.into_iter().map(|v| AsunoyozoraResult { altitude: v.0[0], velocity: v.0[1] }).collect();
+    let js_results: Vec<AsunoyozoraResult> = results
+        .into_iter()
+        .map(|v| AsunoyozoraResult {
+            altitude: v.0[0],
+            velocity: v.0[1],
+        })
+        .collect();
 
     Ok(serde_wasm_bindgen::to_value(&js_results)?)
 }
-
 
 // #[pymodule]
 // fn rust_asunoyozora(m: &Bound<'_, PyModule>) -> PyResult<()> {
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
 mod core;
-use core::{STDATM, asunoyozora, Meter, Kilogram, Second};
+use core::{asunoyozora, Kilogram, Meter, Second, STDATM};
 
 fn main() {
     let t_min = Second(0.0);
-    let t_max = Second(240.0);
+    let t_max = Second(177.0);
     let dt = Second(0.01);
 
-    let tol= Meter(0.001);
+    let tol = Meter(0.001);
 
-    let h_t = Meter(0.45);
-    let h_h = Meter(1.612);
-    let m = Kilogram(60.0);
+    let h_t = Meter(0.30);
+    let h_h = Meter(1.45);
+    let m = Kilogram(43.0);
     let h_width_rate: f64 = 0.23;
 
     let stdatm = STDATM::new(h_t, h_h, m, h_width_rate);
 
+    // print summary of the parameters
+    print!("=== Parameters ===\n");
+    println!("t_min: {} s", t_min.0);
+    println!("t_max: {} s", t_max.0);
+    println!("dt: {} s", dt.0);
+    println!("tol: {} m", tol.0);
+
+    print!("=== Body Parameters ===\n");
+    println!("human thickness: {} m", h_t.0);
+    println!("human height: {} m", h_h.0);
+    println!("human mass: {} kg", m.0);
+    println!("height width rate: {}", h_width_rate);
+
     asunoyozora(t_min, t_max, dt, tol, &stdatm);
 }
-
